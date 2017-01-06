66f4e92da910987d51f66d8e850a3131bbc93ad5
Raj Kamdar <raj9235@gmail.com>
2017-01-07 03:55:50 +0530

    Added Edit_Subjects Activity

---
+++ app/src/main/java/weekender/bunkbuddy/Edit_Subject.java
@@ -0,0 +1,31 @@
+package weekender.bunkbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Edit_Subject extends AppCompatActivity {
+    EditText etName,etPercent;
+    Spinner spinnerInstructor;
+    Button btnEdit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__subject);
+        etName= (EditText) findViewById(R.id.edit_sub_et_name);
+        etPercent= (EditText) findViewById(R.id.edit_sub_at_attendance);
+        spinnerInstructor= (Spinner) findViewById(R.id.edit_sub_spinner);
+        btnEdit= (Button) findViewById(R.id.edit_sub_btn_add);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
