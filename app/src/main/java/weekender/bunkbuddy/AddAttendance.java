d149568a8fba7d926b7a99ce0a4104b87d793a3c
Raj Kamdar <raj9235@gmail.com>
2017-01-08 02:30:23 +0530

    Added Dialog

--- app/src/main/java/weekender/bunkbuddy/AddAttendance.java
+++ app/src/main/java/weekender/bunkbuddy/AddAttendance.java
@@ -1,7 +1,9 @@
 package weekender.bunkbuddy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -57,7 +59,25 @@ public class AddAttendance extends AppCompatActivity {
         add_attendance_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
 
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(AddAttendance.this);
+                builder.setMessage("DID YOU ATTEND THE LECTURE?").setPositiveButton("YES", dialogClickListener)
+                        .setNegativeButton("NO", dialogClickListener).show();
             }
         });
     }
