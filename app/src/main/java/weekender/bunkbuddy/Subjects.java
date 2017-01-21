797f04bbebff7890e446e4663155be7f61b4fc92
Raj Kamdar <raj9235@gmail.com>
2017-01-21 14:49:02 +0530

    Added Subjects DialogClickListener

--- app/src/main/java/weekender/bunkbuddy/Subjects.java
+++ app/src/main/java/weekender/bunkbuddy/Subjects.java
@@ -1,11 +1,14 @@
 package weekender.bunkbuddy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -45,8 +48,35 @@ public class Subjects extends AppCompatActivity {
             }
 
 
+        Subjects_ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
 
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                //Yes button clicked
+
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Subjects.this);
+                builder.setMessage("DID YOU WANT TO DELETE?").setPositiveButton("YES", dialogClickListener)
+                        .setNegativeButton("NO", dialogClickListener).show();
+
+                return false;
+            }
+        });
 
         Subjects_FAB.setOnClickListener(new View.OnClickListener() {
             @Override
